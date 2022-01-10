5522d3f21722c878a808b69a9152917b3b75d104
Venkatvans <97369227+Venkatvans@users.noreply.github.com>
2022-01-10 10:50:31 +0530

    first file

---
+++ jenkins.java
@@ -0,0 +1,12 @@
+/*ip address: 134.209.158.200 
+ http://medeilhq.medeil.io/medeilhq/
+ */
+ 
+ public class jenkins
+ {
+ public static void main(String[] args){
+ System.out.println("hello venkatesh");
+ System.out.println("hello suresh");
+ }
+ }
+ 
